fcc507969e22bce8954cdd2b6b9f66890406af10
sergei <@email>
2026-05-27 22:44:30 +0300

    impl remove ui

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,9 +3,16 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
@@ -18,7 +25,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    var isVisible by rememberSaveable { mutableStateOf(true) }
+                    Row {
+                        Text(
+                            text = "Title always showing",
+                        )
+                        if (isVisible) {
+                            Text(
+                                text = "disappear after click"
+                            )
+                            Button(
+                                onClick = { isVisible = false }
+                            ) {
+                                Text(text = "remove ui")
+                            }
+                        }
+                    }
                 }
             }
         }
