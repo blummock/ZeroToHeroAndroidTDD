6ca07acdb482a78d0753d6dcd84192d2a62e2399
sergei <@email>
2026-05-27 19:13:43 +0300

    impl click me

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,9 +3,11 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
@@ -18,7 +20,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    Text(
+                        modifier = Modifier.clickable(onClick = {}),
+                        text = "Click me!"
+                    )
                 }
             }
         }
